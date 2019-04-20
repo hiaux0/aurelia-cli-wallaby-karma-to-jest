01a0212b947a9fef30527846cf72771a33944b62
hdn <hieudn5293@yahoo.de>
2019-04-20 13:35:00 +0200

    Add testerino component for testing wallaby

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,8 +1,38 @@
-import {App} from '../../src/app';
+import { App } from '../../src/app';
+import 'aurelia-polyfills';
+import { StageComponent } from 'aurelia-testing';
+import { bootstrap } from 'aurelia-bootstrapper';
+import { PLATFORM } from 'aurelia-pal';
 
-describe('the app', () => {
-  it('says hello', () => {
+const view = `
+  <testerino message.bind="message"></testerino>
+`;
+
+const bindings = {
+  message: 'Helloo'
+};
+
+fdescribe('the app', () => {
+  let component;
+  beforeEach(() => {
+    component = StageComponent.withResources(PLATFORM.moduleName('testerino/testerino.html') && PLATFORM.moduleName('testerino/testerino'))
+      .inView(view)
+      .boundTo(bindings);
+  });
+
+  afterEach(() => {
+    component.dispose();
+  });
+
+  it('says hello', async () => {
+    await component.create(bootstrap);
     expect(new App().message).toBe('Hello World!');
+  });
+
+  it('Should bind message in view', async () => {
+    await component.create(bootstrap);
+    const tt = document.querySelector('#testerino');
+    expect(tt.querySelector('#text').textContent.trim()).toBe('Helloo');
   });
 });
 
